844a2556f5814dae9031233bc086f26853a41033
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-10-30 09:41:00 +0200

    UserProfile jsx card component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,8 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
   return (
     <>
       <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Header />
       <MainContent />
