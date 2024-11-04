0ab8879f5471e60c4d6f8c0c2dde2ddfd7239e4d
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-11-04 17:29:51 +0200

    counter implemented

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,7 +5,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-
+import Counter from './components/Counter'
 import UserProfile from './components/UserProfile'
 import './App.css'
 
@@ -18,7 +18,10 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Header />
-      <MainContent />
+      <Counter/>
+      <MainContent 
+     
+      />
       <Footer />
       </div>
       
