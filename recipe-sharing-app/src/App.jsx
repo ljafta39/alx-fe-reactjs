8d65a670103cfc19a83036b1c6b5ae4b23323b2f
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-11-20 15:14:16 +0200

    Fix: dom router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm';
@@ -8,20 +8,22 @@ import EditRecipeForm from './components/EditRecipeForm';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <nav>
-        <Link to="/add">
-          <button>Add Recipe</button>
-        </Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-        <Route path="/edit/:id" element={<EditRecipeForm />} />
-        <Route path="/add" element={<AddRecipeForm />} />
-      </Routes>
-    </div>
+    <Router> {/* Wrap everything in <Router> */}
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <nav>
+          <Link to="/add">
+            <button>Add Recipe</button>
+          </Link>
+        </nav>
+        <Routes> {/* Routes for navigation */}
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/edit/:id" element={<EditRecipeForm />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>  {/* Closing Router here */}
   );
 };
 
