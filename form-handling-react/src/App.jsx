c6c031470eb5ab611d86ede85d10772e6131c96a
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-11-21 15:33:54 +0200

    initial commit for form-handling

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+
+const App = () => (
+  <div>
+    <h1>Controlled Components Example</h1>
+    <RegistrationForm />
+  </div>
+);
+
+export default App;
