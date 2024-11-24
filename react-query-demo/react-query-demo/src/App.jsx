9f59ad15e1b37b96b32d086e59e9d54ddc3611bf
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-11-24 23:24:43 +0200

    router

---
+++ react-query-demo/react-query-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostsComponent from './PostsComponent';
+
+const App = () => {
+  return (
+    <div className="App">
+      <PostsComponent />
+    </div>
+  );
+};
+
+export default App;
