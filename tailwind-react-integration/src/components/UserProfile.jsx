607cd30e51a15824043c4d10d6a2ce3cce7778ce
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-12-01 14:57:15 +0200

    User profile updated with responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,16 @@
 // src/components/UserProfile.jsx
 
 import React from "react";
+
 function UserProfile() {
     return (
-      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         />
-        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center">
+        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
           John Doe
         </h1>
         <p className="text-sm sm:text-base text-gray-600 text-center">
@@ -21,6 +22,27 @@ function UserProfile() {
   }
   
   export default UserProfile;
+//last work task 2
+// function UserProfile() {
+//     return (
+//       <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+//         <img
+//           src="https://via.placeholder.com/150"
+//           alt="User"
+//           className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+//         />
+//         <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center">
+//           John Doe
+//         </h1>
+//         <p className="text-sm sm:text-base text-gray-600 text-center">
+//           Developer at Example Co. Loves to write code and explore new
+//           technologies.
+//         </p>
+//       </div>
+//     );
+//   }
+  
+//   export default UserProfile;
 
 //task 1
 // import React from "react";
