99f4684381ec598da20167110e8726e7819f2c7d
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-12-01 14:53:44 +0200

    User profile updated with imgaes sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,14 +1,13 @@
 // src/components/UserProfile.jsx
 
 import React from "react";
-
 function UserProfile() {
     return (
       <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 md:w-40 md:h-40 mx-auto"
+          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
         />
         <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center">
           John Doe
@@ -22,7 +21,6 @@ function UserProfile() {
   }
   
   export default UserProfile;
-
 
 //task 1
 // import React from "react";
