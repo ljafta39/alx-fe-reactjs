384f2b8f47e8269ded460fb76fede04daab612f3
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-12-01 15:35:13 +0200

    router useauth

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from './useAuth';
 
-function ProtectedRoute({ isAuthenticated, children }) {
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
   return isAuthenticated ? children : <Navigate to="/" />;
 }
 
