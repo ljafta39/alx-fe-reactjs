2a4dff9f151ef80f5a2bdeac05075e7a250e727b
Lungelihle Jafta <Lungelihlej@gmail.com>
2024-12-02 12:09:30 +0200

    initial commit for receipe platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-500'>Testing tailwind</h1>
+      
+    </>
+  )
+}
+
+export default App
